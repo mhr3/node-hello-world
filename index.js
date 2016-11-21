c3b47a08f43160ece6eb27088dc4562e8e6c348c
Michal Hruby <michal.hruby@xamarin.com>
2016-11-21 22:30:12 +0000

    Fix crash

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 const rand = Math.random().toString().substr(2, 8);
-require('http').createServer((req, res) => res.end(`hello world ${rand} ${Date.now()}\nrequest: ${JSON.stringify(req)}`)).listen(3000);
+require('http').createServer((req, res) => res.end(JSON.stringify({now: Date.now(), rand: rand, method: req.method, URL: req.url, headers: req.headers}))).listen(3000);
 
 
