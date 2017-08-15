a5b21b2c2f7745ff35ab23788b034295e2b58c0a
Michal Hruby <michal.mhr@gmail.com>
2017-08-15 13:55:44 +0100

    Log requests

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 
 const rand = Math.random().toString().substr(2, 8);
-require('http').createServer((req, res) => res.end(JSON.stringify({now: Date.now(), rand: rand, method: req.method, URL: req.url, headers: req.headers}))).listen(3000);
+require('http').createServer((req, res) => {
+  var receivedBytes = 0
+  console.log("[request]", req.method, req.url, "- from", req.socket.remoteAddress + ":" + req.socket.remotePort);
+  req.on('data', (chunk) => {
+    receivedBytes += chunk.length;
+  });
+  req.on('end', () => {
+    res.end(JSON.stringify({
+      now: Date.now(),
+      rand: rand,
+      method: req.method,
+      requestLength: receivedBytes,
+      URL: req.url,
+      headers: req.headers
+    }));
+  })
+}).listen(3000);
 
 
